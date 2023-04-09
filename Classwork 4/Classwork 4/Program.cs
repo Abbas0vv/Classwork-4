78026338896aba80b251da8734682b26766642dc
User <User@WINDOWS-2R2MKCU>
2023-04-09 23:28:53 +0400

    3/5 done

--- Classwork 4/Classwork 4/Program.cs
+++ Classwork 4/Classwork 4/Program.cs
@@ -2,190 +2,229 @@
 
 namespace Testing
 {
-    public class UserDatas
+    public class DataBase
     {
-        public void AllDatas()
+        public List<string> userName;
+        public List<string> userSecondName;
+        public List<string> userEmail;
+        public List<string> userPassword;
+
+        public DataBase()
         {
-        public List<string> userName = new List<string>();
-        public List<string> userSecondName = new List<string>();
-        public List<string> userEmail = new List<string>();
-        public List<string> userPassword = new List<string>();
+            userName = new List<string>();
+            userSecondName = new List<string>();
+            userEmail = new List<string>();
+            userPassword = new List<string>();
+        }
     }
-}
-}
-
-internal class Program
-{
-    static void Main(string[] args)
+    internal class Program
     {
-        UserDatas UD = new UserDatas();
-        UD.AllDatas();
-
-        int login = 0;
-
-        while (true)
+        #region Menu
+        static string Menu()
         {
-            string option;
-            if (login == 0)
-            {
-                Console.Write("Which option do you need?  1.Register  3.Exit : ");
-                option = Console.ReadLine()!;
-            }
-            else
-            {
-                Console.Write("Which option do you need?  1.Register  2.Login  3.Exit : ");
-                option = Console.ReadLine()!;
-            }
 
-            if (option == "1" || option == "Register")
+            Console.Write("Which option do you need?  1.Register  2.Login  3.Exit : ");
+            return Console.ReadLine()!;
+        }
+        #endregion
+
+        static void Main(string[] args)
+        {
+            StartProgramLoop();
+        }
+
+
+        #region Starting Program
+        static void StartProgramLoop()
+        {
+            while (true)
             {
-                while (true)
+                string choise = Menu();
+                if (choise == "1" || choise == "Register")
                 {
-                    Console.Write("Pls enter first name : ");
-                    string inputName = Console.ReadLine()!;
+                    OperationOne();
+                }
+                else if (choise == "2" || choise == "Login")
+                {
+                    OperationTwo();
+                }
+                else if (choise == "3" || choise == "Exit")
+                {
+                    ExitCommand();
+                    break;
+                }
+            }
+        }
+        #endregion
 
-                    if (inputName.Length > 3 || inputName.Length < 30)
+        #region First Operation
+        static void OperationOne()
+        {
+            DataBase lists = new DataBase();
+
+            lists.userName.Add(IsValidName());
+            lists.userSecondName.Add(IsValidSecondName());
+            lists.userEmail.Add(IsValidEmail());
+            lists.userPassword.Add(IsValidPassword());
+        }
+        static string IsValidName()
+        {
+            Console.Write("Pls enter first name : ");
+            string inputName = Console.ReadLine()!;
+
+            while (true)
+            {
+                if (inputName.Length > 3 || inputName.Length < 30)
+                {
+                    return inputName;
+                }
+                else
+                {
+                    Console.WriteLine("Some informations are incorrect!");
+                    Console.WriteLine();
+                    Console.WriteLine("Pls enter first name : ");
+                    inputName = Console.ReadLine()!;
+                }
+            }
+        }
+        static string IsValidSecondName()
+        {
+            Console.Write("Pls enter second name : ");
+            string inputSecondName = Console.ReadLine()!;
+
+            while (true)
+            {
+                if (inputSecondName.Length > 5 || inputSecondName.Length < 20)
+                {
+                    return inputSecondName;
+                }
+                else
+                {
+                    Console.WriteLine("Some informations are incorrect!");
+                    Console.WriteLine();
+                    Console.WriteLine("Pls enter second name");
+                    inputSecondName = Console.ReadLine()!;
+                }
+            }
+        }
+        static string IsValidEmail()
+        {
+            Console.Write("Pls enter email : ");
+            string inputEmail = Console.ReadLine()!;
+
+            while (true)
+            {
+                for (int i = 0; i < inputEmail.Length; i++)
+                {
+                    if (inputEmail[i] == '@')
                     {
-                        userName.Add(inputName);
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Some informations are incorrect!");
-                        Console.WriteLine();
+                        return inputEmail;
                     }
                 }
+                Console.WriteLine("Some informations are incorrect!");
+                Console.WriteLine();
+                Console.WriteLine("Pls enter email");
+                inputEmail = Console.ReadLine()!;
+            }
+        }
+        static string IsValidPassword()
+        {
+            Console.WriteLine("Pls create a password : ");
+            string firstPassword = Console.ReadLine()!;
+            Console.WriteLine("Pls enter your password again : ");
+            string secondPassword = Console.ReadLine()!;
 
-                while (true)
+            while (true)
+            {
+                if (firstPassword == secondPassword)
                 {
-                    Console.Write("Pls enter second name : ");
-                    string inputSecondName = Console.ReadLine()!;
-
-                    if (inputSecondName.Length < 20 || inputSecondName.Length > 5)
-                    {
-                        userSecondName.Add(inputSecondName);
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Some informations are incorrect!");
-                        Console.WriteLine();
-                    }
+                    string password = firstPassword;
+                    return password;
                 }
+                Console.WriteLine("Some informations are incorrect!");
+                Console.WriteLine();
+                Console.WriteLine("Pls create a password : ");
+                firstPassword = Console.ReadLine()!;
+                Console.WriteLine("Pls enter your password again : ");
+                secondPassword = Console.ReadLine()!;
+            }
+        }
+        #endregion
 
-                while (true)
+        #region Second Operation
+        static void OperationTwo()
+        {
+            IsValidLogin();
+        }
+        static void IsValidLogin()
+        {
+            DataBase lists = new DataBase();
+            DataBase dataBase = new DataBase();
+
+            bool checkAll = false;
+            bool checkEmail = false;
+            bool checkPassword = false;
+            int checkBox = -1;
+
+            Console.Write("Pls enter your email : ");
+            string email = Console.ReadLine()!;
+
+            Console.Write("Pls enter your password : ");
+            string password = Console.ReadLine()!;
+
+            foreach (string ValidEmail in lists.userEmail)
+            {
+                if (ValidEmail == email)
                 {
-                    bool check = false;
-                    Console.Write("Pls enter email : ");
-                    string inputEmail = Console.ReadLine()!;
-
-                    for (int i = 0; i < inputEmail.Length; i++)
+                    for (int i = 0; i < lists.userEmail.Count; i++)
                     {
-                        if (inputEmail[i] == '@')
+                        if (lists.userEmail[i] == email)
                         {
-                            for (int j = 0; j < inputEmail.Length; j++)
-                            {
-                                if (inputEmail[j] == '.')
-                                {
-                                    userEmail.Add(inputEmail);
-                                    check = true;
-                                    break;
-                                }
-                            }
-                        }
-                        if (check)
-                        {
+                            checkEmail = true;
+                            checkBox = i; 
                             break;
                         }
                     }
-                    if (check)
-                    {
-                        break;
-                    }
-                    Console.WriteLine("Some informations are incorrect!");
-                    Console.WriteLine();
-                }
-
-                while (true)
-                {
-                    Console.WriteLine("Pls create a password : ");
-                    string firstPassword = Console.ReadLine()!;
-                    Console.WriteLine("Pls enter your password again : ");
-                    string secondPassword = Console.ReadLine()!;
-
-                    if (firstPassword == secondPassword)
-                    {
-                        string password = firstPassword;
-                        userPassword.Add(password);
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Some informations are incorrect!");
-                        Console.WriteLine();
-                    }
                 }
             }
-            else if (option == "2" || option == "Login")
+
+            if (checkBox != -1 && lists.userPassword[checkBox] == password)
             {
-                bool checkAll = false;
-                bool checkEmail = false;
-                bool checkPassword = false;
-                int checkBox = 0;
-
-                while (true)
-                {
-                    Console.Write("Pls enter your email : ");
-                    string email = Console.ReadLine()!;
-
-                    Console.Write("Pls enter your password : ");
-                    string password = Console.ReadLine()!;
-
-                    foreach (string ValidEmail in userEmail)
-                    {
-                        if (ValidEmail == email)
-                        {
-                            for (int i = 0; i < userEmail.Count; i++)
-                            {
-                                if (userEmail[i] == email)
-                                {
-                                    checkBox = i; break;
-                                }
-                            }
-                            checkEmail = true;
-                        }
-                    }
-
-                    if (userPassword[checkBox] == password)
-                    {
-                        checkPassword = true;
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Someting went wrong! Try again");
-                        continue;
-                    }
-                }
-
-                if (checkEmail && checkPassword)
-                {
-                    checkAll = true;
-                }
-
-                if (checkAll)
-                {
-                    Console.WriteLine($"Welcome {userName[checkBox]} {userSecondName[checkBox]}!");
-                }
+                checkPassword = true;
             }
-            else if (option == "3" || option == "Exit")
+            else
             {
-                Console.WriteLine("Thank you!");
-                break;
+                Console.WriteLine("Someting went wrong! Try again");
             }
-
-            login++;
+            CompletedLogin(checkAll, checkEmail, checkPassword, checkBox, dataBase);
         }
+        static void CompletedLogin(bool checkAll, bool checkEmail, bool checkPassword, int checkBox, DataBase dataBase)
+        {
+            IsValidEmail();
+
+            if (checkEmail == true && checkPassword == true)
+            {
+                checkAll = true;
+            }
+
+            if (checkAll)
+            {
+                Console.WriteLine($"Welcome {dataBase.userName[checkBox]} {dataBase.userSecondName[checkBox]}!");
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong!");
+                Console.WriteLine();
+                IsValidLogin();
+            }
+        }
+        #endregion
+
+        #region Last Operation
+        static void ExitCommand()
+        {
+            Console.WriteLine("Thank you!");
+        }
+        #endregion
     }
-}
 }
